6abaaebb0f2a739e7f872b485c0c042b0201aadd
bootcamp <bootcamp@home.venturus.local>
2018-06-30 12:05:01 -0300

    sample input

--- bootcamp-teste/src/app/App.kt
+++ bootcamp-teste/src/app/App.kt
@@ -1,26 +1,50 @@
 package app
 
+import kotlinx.html.js.onChangeFunction
 import react.*
 import react.dom.*
 import logo.*
 import ticker.*
 
-class App : RComponent<RProps, RState>() {
+class App : RComponent<RProps, App.State>() {
+
+    class State(var inputValue: String) : RState
+
+    override fun componentWillMount() {
+        setState {
+            state.inputValue = "Input your name"
+        }
+    }
+
+
     override fun RBuilder.render() {
+
+        h1 {
+            +"Hello Bootcamp"
+        }
+
+
         div("App-header") {
-            logo()
-            h2 {
-                +"Welcome to React with Kotlin"
+            input {
+                attrs {
+                    value = state.inputValue
+                    onChangeFunction =  { event ->
+                        val value = event.currentTarget.asDynamic().value
+                        setState {
+                            inputValue = value
+                        }
+
+                    }
+                }
             }
         }
-        p("App-intro") {
-            +"To get started, edit "
-            code { +"app/App.kt" }
-            +" and save to reload."
+
+        div {
+            span {
+                +state.inputValue
+            }
         }
-        p("App-ticker") {
-            ticker()
-        }
+
     }
 }
 
